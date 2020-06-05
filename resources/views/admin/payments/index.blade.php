ad12b2d0feab19ed048e56d99f4f8a5c883f3af8
MrMicky <git@mrmicky.fr>
2020-06-05 16:21:43 +0200

    Add payments search

--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -3,6 +3,22 @@
 @section('title', trans('shop::admin.payments.title'))
 
 @section('content')
+    <form class="form-inline mb-3" action="{{ route('shop.admin.payments.index') }}" method="GET">
+        <div class="form-group mb-2">
+            <label for="searchInput" class="sr-only">{{ trans('messages.actions.search') }}</label>
+
+            <div class="input-group">
+                <input type="text" class="form-control" id="searchInput" name="search" value="{{ $search ?? '' }}" placeholder="{{ trans('messages.actions.search') }}">
+
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-search fa-sm"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+
     <div class="card shadow mb-4">
         <div class="card-body">
 
@@ -38,7 +54,7 @@
                     </tbody>
                 </table>
 
-                {{ $payments->links() }}
+                {{ $payments->withQueryString()->links() }}
 
             </div>
         </div>
--- src/Controllers/Admin/PaymentController.php
+++ src/Controllers/Admin/PaymentController.php
@@ -4,6 +4,8 @@ namespace Azuriom\Plugin\Shop\Controllers\Admin;
 
 use Azuriom\Http\Controllers\Controller;
 use Azuriom\Plugin\Shop\Models\Payment;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
@@ -12,10 +14,25 @@ class PaymentController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $search = $request->input('search');
+
+        $payments = Payment::notPending()
+            ->with('user')
+            ->latest()
+            ->when($search, function (Builder $query, string $search) {
+                $query->where('payment_id', 'LIKE', "%{$search}%");
+
+                if (is_numeric($search)) {
+                    $query->orWhere('id', $search)
+                        ->orWhere('user_id', 'LIKE', "%{$search}%");
+                }
+            })->paginate();
+
         return view('shop::admin.payments.index', [
-            'payments' => Payment::notPending()->with('user')->latest()->paginate(),
+            'payments' => $payments,
+            'search' => $search,
         ]);
     }
 }
