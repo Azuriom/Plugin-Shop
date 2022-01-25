cd79090a845c20e947f437704cf900f6f7a6af9c
MrMicky <git@mrmicky.fr>
2022-01-25 23:09:50 +0100

    Trailing space...

--- src/Controllers/Admin/StatisticsController.php
+++ src/Controllers/Admin/StatisticsController.php
@@ -16,7 +16,7 @@ class StatisticsController extends Controller
         $gatewaysPayments = Gateway::all()->map(function (Gateway $gateway) {
             $query = $this->getCompletedPayments()
                 ->where('gateway_type', '=', $gateway->type);
-            $method =  payment_manager()->getPaymentMethod($gateway->type);
+            $method = payment_manager()->getPaymentMethod($gateway->type);
 
             return [
                 'name' => $method ? $method->name() : $gateway->type,
