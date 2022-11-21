295d4fddfcd0d754618db8a5373f7f06cb6834d3
lapetitehanaé <51682613+lapetitehanae@users.noreply.github.com>
2022-11-21 01:33:51 +0100

    Update index.blade.php

--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -51,7 +51,7 @@
                                     {{ trans('shop::admin.payments.status.'.$payment->status) }}
                                 </span>
                             </td>
-                            <td>{{ $payment->transaction_id ?? trans('messages.unknown') }}</td>
+                            <td>{{ $payment->transaction_id ?? trans( $payment->id ) }}</td>
                             <td>{{ format_date_compact($payment->created_at) }}</td>
                             <td>
                                 <a href="{{ route('shop.admin.payments.show', $payment) }}" class="mx-1" title="{{ trans('messages.actions.show') }}" data-bs-toggle="tooltip"><i class="bi bi-eye"></i></a>
