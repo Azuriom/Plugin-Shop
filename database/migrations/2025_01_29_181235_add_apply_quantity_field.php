97acebd2abbf171077ae292407477d1da2ec9231
Pierre-Olivier GOIN <60008676+Koora7334@users.noreply.github.com>
2025-01-29 19:27:37 +0100

    Add migration to add the apply_quantity field

---
+++ database/migrations/2025_01_29_181235_add_apply_quantity_field.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('shop_packages')->get()->each(function ($command) {
+            $commands = json_decode($command->commands, true);
+
+            foreach ($commands as $key => &$commandItem) {
+                if (!isset($commandItem['apply_quantity'])) {
+                    $commandItem['apply_quantity'] = 0;
+                }
+            }
+
+            DB::table('shop_packages')
+                ->where('id', $command->id)
+                ->update(['commands' => json_encode($commands)]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('shop_packages')->get()->each(function ($command) {
+            $commands = json_decode($command->commands, true);
+
+            foreach ($commands as $key => &$commandItem) {
+                if (isset($commandItem['apply_quantity'])) {
+                    unset($commandItem['apply_quantity']);
+                }
+            }
+
+            DB::table('shop_packages')
+                ->where('id', $command->id)
+                ->update(['commands' => json_encode($commands)]);
+        });
+    }
+};
