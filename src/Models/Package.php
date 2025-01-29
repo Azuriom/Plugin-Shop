b9cc68ecf9c0669975d5f62a456592224f28dcb7
Pierre-Olivier GOIN <60008676+Koora7334@users.noreply.github.com>
2025-01-29 17:02:41 +0100

    Fix duplicates commands based on quantity

--- src/Models/Package.php
+++ src/Models/Package.php
@@ -462,7 +462,6 @@ class Package extends Model implements Buyable
                 $item->quantity, $this->id, $this->name, $item->price, $item->payment->transaction_id,
             ], $command))
             ->map(fn (string $command) => $item->replaceVariables($command))
-            ->flatMap(fn (string $command) => array_fill(0, $item->quantity, $command))
             ->all();
     }
 }
